d9e8105b690a7316dacd1fdc5b81f1490632143a
fre556 <fre556@yahoo.com>
2021-05-01 05:10:39 +0100

    update

---
+++ app/src/main/java/com/blah/project/activities/validate.java
@@ -0,0 +1,20 @@
+package com.blah.project.activities;
+
+import android.support.design.widget.TextInputEditText;
+
+/**
+ * Created by Beast on 19/04/2018.
+ */
+
+public class validate {
+
+
+
+
+    public boolean validateemail(TextInputEditText textInputEditText) {
+
+        String email = textInputEditText.toString().trim();
+
+        return true;
+    }
+}
